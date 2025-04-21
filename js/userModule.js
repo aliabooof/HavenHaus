19b0fbf3c5c2ac3d36f26f04d9fca7a16cab2b3e
Ali Abooof <aliabooof@outlook.com>
2025-04-21 21:46:27 +0200

    handling the unique id issue

--- js/userModule.js
+++ js/userModule.js
@@ -1,3 +1,4 @@
+import {GetTable } from "./db.js"
 export class User{
     constructor({ firstName, lastName, email, role, phone, password }) {
         this.firstName = firstName;
@@ -6,14 +7,27 @@ export class User{
         this.role = role;
         this.phone = phone;
         this.password = password;
-        this.id = this.generateId(firstName, lastName);
+        this.id = this.#generateUniqueId(firstName,lastName);
     }
 
-    generateId(firstName , lastName) {
-        return firstName + lastName + '_' + Math.random().toString(15).substring(2, 9);
+    #generateId(firstName , lastName) {
+        return firstName + lastName + '_' + Math.random().toString(32).substring(2, 9);
     }
 
- 
+    #generateUniqueId(firstName , lastName){
+        let id;
+        const existingIds = User.getAllExistingUserIds();
+        do{
+            console.log("heyyy");
+            id = this.#generateId(firstName , lastName);
+        }while(existingIds.includes(id));
+        return id;
+    }
+
+    static getAllExistingUserIds() {
+        const users = GetTable("user") || [];
+        return users.map(user => user.id);
+    }
 
    
 } 
