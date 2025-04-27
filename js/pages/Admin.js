53a9f9bd5a7822809e66a94e5c725dfe570e13f4
mariamkashkoush <mariamkashkoush26@gmail.com>
2025-04-27 21:47:22 +0300

    SearchbarFilter

--- js/pages/Admin.js
+++ js/pages/Admin.js
@@ -27,3 +27,19 @@ async function loadContent(x) {
 document.getElementById("user_button").addEventListener("click", async () => {
     await loadContent(2);
 })
+
+function filterTable() {
+    const searchInput = document.getElementById("searchInput").value.toLowerCase();
+    const rows = document.querySelectorAll("#userTableBody tr");
+
+    rows.forEach(row => {
+        const name = row.cells[1].textContent.toLowerCase(); // Get the user's name in lowercase
+        if (name.includes(searchInput)) {
+          row.style.display = ""; // Show the row if it matches the search query
+        } else {
+          row.style.display = "none"; // Hide the row if it doesn't match
+        }
+    });
+
+
+}
